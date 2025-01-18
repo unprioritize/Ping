69d01949a447bdc9a1321dee9f42c2648cb8cd4e
priority <144442238+unprioritize@users.noreply.github.com>
2025-01-18 13:23:08 +0200

    Update ping.js

--- ping.js
+++ ping.js
@@ -2,7 +2,7 @@ module.exports = function({bot, commands}) {
         commands.addGlobalCommand ('ping', '',(msg, args) => {
 		bot.createMessage(msg.channel.id, ":ping_pong: *Pinging...*").then(m =>{
 			const ping = m.timestamp - msg.timestamp;
-			m.edit(':ping_pong: **Pong!** Your ping is __`${ping}ms`__.');
+			m.edit(":ping_pong: **Pong!** Your ping is __`${ping}ms`__.");
 		});
 	});
 };
